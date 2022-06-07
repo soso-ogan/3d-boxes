d971d7f0f420761ad7b6319d0cca5683d7dc8f88
nbogie <neillbogie@googlemail.com>
2022-06-07 23:07:53 +0100

    performance improvements - share geom and materials wherever possible

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
 import {
-    Scene,
-    Mesh,
-    MeshStandardMaterial,
-    Color,
-    BoxGeometry,
-    TorusGeometry,
-    ConeGeometry,
-    Group,
-    CylinderGeometry,
+    BoxGeometry, BufferGeometry, Color, ConeGeometry, CylinderGeometry, Group, Material, Mesh,
+    MeshStandardMaterial, Scene
 } from 'three';
-import { randFloat, randInt } from 'three/src/math/MathUtils';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { randFloat, randFloatSpread, randInt } from 'three/src/math/MathUtils';
 import { setupCamera } from './setupCamera';
 import { setupHelpers } from './setupHelpers';
 import { setupLights } from './setupLights';
 import { setupOrbitControls } from './setupOrbitControls';
 import { setupRenderer } from './setupRenderer';
-import Stats from 'three/examples/jsm/libs/stats.module'
-
-const stats = Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom);
+import { setupStatsPanel } from './setupStatsPanel';
 
 export function setupThreeJSScene() {
 
@@ -28,7 +18,7 @@ export function setupThreeJSScene() {
     const camera = setupCamera(dimensions); //can move
 
     const renderer = setupRenderer(camera, dimensions);
-
+    const stats = setupStatsPanel();
     const controls = setupOrbitControls(camera, renderer.domElement);
     controls.autoRotate = false;
     let scene = new Scene();
@@ -36,32 +26,15 @@ export function setupThreeJSScene() {
     setupLights(scene);
     setupHelpers(scene);
 
-    const myOrangeFloor = orangeFloor()
-    myOrangeFloor.position.y = -1
-    scene.add(myOrangeFloor)
+    createFloor(scene)
 
-    for (let i = 0; i < 150; i++) {
-        const pineTree = createPineTree()
-        pineTree.scale.multiplyScalar(randFloat(1 / 5, 1))
-        scene.add(pineTree)
-    }
+    createPineTrees(scene)
 
-    for (let i = 0; i < 10000; i++) {
-        const grass = createGrass()
-
-        // scene.add(pineTree)
-        scene.add(grass)
-    }
-
+    createFieldOfGrass(scene);
     animate();
 
     function animate() {
-        // myShape.rotation.y += 0.01;
-        // myShape.rotation.x += 0.02;
-
         renderer.render(scene, camera);
-
-        // required if controls.enableDamping or controls.autoRotate are set to true
         controls.update();
         stats.update()
         requestAnimationFrame(animate);
@@ -69,49 +42,25 @@ export function setupThreeJSScene() {
 }
 setupThreeJSScene();
 
-function orangeFloor(): Mesh {
-
-    //shape(s)
-    const geometry = new BoxGeometry(100, .5, 100);
-    const colour1 = new Color(0xBB6B00);
+function createFloor(scene: Scene): void {
+    const floorThickness = 0.5;
+    const geometry = new BoxGeometry(100, floorThickness, 100);
+    const floorColour = new Color("#274a26");
     const material = new MeshStandardMaterial({
-        color: colour1
+        color: floorColour
     });
-    let myShape: Mesh = new Mesh(geometry, material);
-    myShape.position.y = 0;
-    return myShape
-    // scene.add(myShape);
+    let myFloorMesh: Mesh = new Mesh(geometry, material);
+    myFloorMesh.position.y = -floorThickness / 2;//so that top of floor box touches 0 on y.
+    scene.add(myFloorMesh)
 }
 
-function createDonut(): Mesh {
 
-    //shape(s)
-    const geometry = new TorusGeometry(10, 3, 10, 100);
-    const colour1 = new Color('skyBlue');
-    const material = new MeshStandardMaterial({
-        color: colour1
-    });
-
-    let myShape: Mesh = new Mesh(geometry, material);
-    myShape.position.y = randomAroundZero(50);
-    myShape.position.z = randomAroundZero(50);
-    myShape.position.x = randomAroundZero(50);
-    myShape.rotation.x = Math.PI / 2;
-    return myShape
-    // scene.add(myShape);
-    //documentation for donut
-    //https://threejs.org/docs/?q=geometr#api/en/geometries/TorusGeometry
-}
-function randomAroundZero(half: number): number {
-    return Math.random() * half * 2 - half
-};
-
-function createPineTree(): Group {
-
+function createPineTrees(scene: Scene): void {
+    //make geometries and materials which all trees will share
     const topOfTreeGeometry = new ConeGeometry(8, 12, 8);
     const middleOfTreeGeometry = new ConeGeometry(13, 17, 8);
     const bottomOfTreeGeometry = new ConeGeometry(17, 22, 8);
-    const treeTruncGeometry = new CylinderGeometry(5, 5, 20, 30);
+    const treeTruncGeometry = new CylinderGeometry(5, 5, 20, 8);
 
     const treeColour = new Color(0x244F26);
     const treeTruncColour = new Color(0x522B29);
@@ -119,47 +68,87 @@ function createPineTree(): Group {
         color: treeColour,
     })
     const truncMaterial = new MeshStandardMaterial({
-        color: treeTruncColour
+        color: treeTruncColour,
+        flatShading: true
     })
+
+    for (let i = 0; i < 150; i++) {
+        const group = createOnePineTree(topOfTreeGeometry, middleOfTreeGeometry, bottomOfTreeGeometry, treeTruncGeometry, treeMaterial, truncMaterial);
+        scene.add(group)
+    }
+
+
+
+}
+function createOnePineTree(topOfTreeGeometry: BufferGeometry, middleOfTreeGeometry: BufferGeometry, bottomOfTreeGeometry: BufferGeometry, treeTruncGeometry: BufferGeometry, treeMaterial: Material, truncMaterial: Material): Group {
 
     const fullPineTree = new Group()
     const topOfPineTree = new Mesh(topOfTreeGeometry, treeMaterial)
-    topOfPineTree.position.y = 20
+    topOfPineTree.position.y = 40
+    topOfPineTree.rotation.y = randFloatSpread(Math.PI);
     fullPineTree.add(topOfPineTree)
 
     const middleOfPineTree = new Mesh(middleOfTreeGeometry, treeMaterial)
-    middleOfPineTree.position.y = 10
+    middleOfPineTree.position.y = 30
+    middleOfPineTree.rotation.y = randFloatSpread(Math.PI);
     fullPineTree.add(middleOfPineTree)
 
     const bottomOfPineTree = new Mesh(bottomOfTreeGeometry, treeMaterial)
-    bottomOfPineTree.position.y = 0
+    bottomOfPineTree.position.y = 20
+    bottomOfPineTree.rotation.y = randFloatSpread(Math.PI);
     fullPineTree.add(bottomOfPineTree)
 
     const truncOfTree = new Mesh(treeTruncGeometry, truncMaterial)
-    truncOfTree.position.y = -10
+    truncOfTree.position.y = 10
     fullPineTree.add(truncOfTree)
 
-    fullPineTree.position.y = 5
-    fullPineTree.scale.set(.25, .25, .25)
-    fullPineTree.position.x = randomAroundZero(50)
-    fullPineTree.position.z = randomAroundZero(50)
-    return fullPineTree
+    //We can use this red ball to show us where the pivot point / origin of the group is/
+    // const ball = new Mesh(new SphereGeometry(10, 5, 5),
+    //     new MeshStandardMaterial({ color: new Color("red"), transparent: true, opacity: 0.5 }));
+    // fullPineTree.add(ball);
+    fullPineTree.scale.multiplyScalar(0.25);
+    fullPineTree.position.x = randFloatSpread(100)
+    fullPineTree.position.z = randFloatSpread(100)
+
+    fullPineTree.scale.multiplyScalar(randFloat(1 / 5, 1))
+    //Give each tree a random turn about the y axis (so segments don't all line up)
+    fullPineTree.rotation.y = randFloatSpread(Math.PI);
+
+    //give each tree a very slight random lean
+    fullPineTree.rotation.x = randFloatSpread(0.02);
+    fullPineTree.rotation.z = randFloatSpread(0.02);
+    return fullPineTree;
+
 }
 
-function createGrass(): Mesh {
-    const grassColour1 = new Color(0x2F9C95)
-    const grassColour2 = new Color(0x40C9A2)
-    const grassColour3 = new Color(0xA3F7B5)
-    const grassColours = [grassColour1, grassColour2, grassColour3]
-    const randomGrassColour = grassColours[randInt(0, grassColours.length)]
-    const grassMaterial = new MeshStandardMaterial({
-        color: randomGrassColour
-    })
-    const grassGeometry = new BoxGeometry(.15, randomAroundZero(4), .15)
-    const grassMesh = new Mesh(grassGeometry, grassMaterial)
-    grassMesh.position.y = 2
-    grassMesh.position.x = randomAroundZero(50)
-    grassMesh.position.z = randomAroundZero(50)
-    return grassMesh
+
+function createFieldOfGrass(scene: Scene): void {
+    const grassColours = [0x2F9C95, 0x40C9A2, 0xA3F7B5]
+    const grassMaterials: Material[] = grassColours.map(c => new MeshStandardMaterial({
+        color: c
+    }));
+
+    const grassBasicLength = 2;
+    const grassGeometry = new BoxGeometry(.15, grassBasicLength, .15)
+    for (let i = 0; i < 10000; i++) {
+        const chosenMaterial = pick(grassMaterials)
+        const grassMesh = new Mesh(grassGeometry, chosenMaterial)
+        grassMesh.position.x = randFloatSpread(100)
+        grassMesh.position.z = randFloatSpread(100)
+        grassMesh.rotation.y = randFloat(0, Math.PI * 2)
+        grassMesh.rotation.z = randFloatSpread(0.2)
+        grassMesh.rotation.x = randFloatSpread(0.2)
+
+        const yScale = randFloat(0.2, 1);
+        grassMesh.scale.y = yScale;
+        //move it up half of its height, so it extends down to touch the ground
+        grassMesh.position.y = grassBasicLength * yScale / 2;
+        scene.add(grassMesh)
+    }
+
 }
 
+function pick<ElementType>(arr: ElementType[]): ElementType {
+    const index = randInt(0, arr.length - 1)
+    return arr[index];
+}
