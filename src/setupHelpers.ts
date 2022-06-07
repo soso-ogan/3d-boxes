809030f4ba34b15f675b0af3b2c5d95cf6aab1b7
nbogie <neillbogie@googlemail.com>
2022-06-07 23:07:28 +0100

    move helpers so they can be seen

--- src/setupHelpers.ts
+++ src/setupHelpers.ts
@@ -2,8 +2,9 @@ import { Scene, AxesHelper, GridHelper } from "three";
 
 export function setupHelpers(scene: Scene) {
     const axesHelper = new AxesHelper(10);
-    axesHelper.position.set(0, 1, 0); //lift up from grid
+    axesHelper.position.set(-40, 20, 0); //lift up from grid
     scene.add(axesHelper);
     const gridHelper = new GridHelper(100);
+    gridHelper.position.y = 1;
     scene.add(gridHelper);
 }
